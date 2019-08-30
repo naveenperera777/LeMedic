43d4b6f4353aa3457be1820b3aed6345f6f55b29
Naveen Perera <naveen@arimaclanka.com>
2019-08-30 11:01:40 +0530

    Add patient record service

---
+++ medic/src/main/java/com/medic/MainApp/Controllers/PatientRecordController.java
@@ -0,0 +1,54 @@
+package com.medic.MainApp.Controllers;
+
+
+import com.medic.MainApp.Models.PatientRecord;
+import com.medic.MainApp.Services.PatientRecordService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class PatientRecordController {
+
+    private static final Logger logger = LoggerFactory.getLogger(PatientController.class);
+    private final PatientRecordService patientRecordService;
+
+
+    public PatientRecordController(PatientRecordService patientRecordService){
+        this.patientRecordService = patientRecordService;
+    }
+//
+//    //Get a patient record by id
+//    @GetMapping("/record/{id}")
+//    public ResponseEntity getPatientRecordById(String id){
+//        List patientRecord = patientRecordService.getPatientRecordById(id);
+//    }
+//
+//    //Get all records for a particular patient
+//    @GetMapping("/record/patient/{id}")
+//    public ResponseEntity getAllRecordsOfAPatient(String userId){
+//        List patientRecords = patientRecordService.getAllRecordsOfAPatient(userId);
+//    }
+//
+//    //Get all patient records
+//    @GetMapping("/record/patient/all")
+//    public ResponseEntity getAllPatientRecords(){
+//        List AllPatientRecords = patientRecordService.getAllPatientRecords();
+//    }
+//
+//    //Save a patient record
+//    @PostMapping("/record/save")
+//    public ResponseEntity savePatientRecord(PatientRecord patientRecord){
+//        patientRecordService.savePatientRecord;
+//    }
+
+
+
+}
---
+++ medic/src/main/java/com/medic/MainApp/Services/PatientRecordService.java
@@ -0,0 +1,7 @@
+package com.medic.MainApp.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientRecordService {
+}
